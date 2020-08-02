0e065961415d67b6d27cbfa28d3d802e6b27ac88
Cezary Waś <kontakt@cezarywas.pl>
2020-08-02 20:59:45 +0200

    DC#19 final I hope

--- Daily Challenge #18 - Triple Trouble/triple_trouble.py
+++ Daily Challenge #18 - Triple Trouble/triple_trouble.py
@@ -1,6 +1,37 @@
 def tripledouble(num1, num2):
+    num1 = str(num1)
+    num1 = list(num1)
+    num2 = str(num2)
+    num2 = list(num2)
+    first_number = []
+    second_number = []
+    final = []
+    i = 0
+    while i < len(num1) - 1:
+        if num1[i] == num1[i+1] and num1[i+1] == num1[i+2]:
+            first_number.append(num1[i])
+            i += 3
+        else:
+            i += 1
+    i = 0
+    while i < len(num2) - 1:
+        if num2[i] == num2[i+1]:
+            second_number.append(num2[i])
+            i += 2
+        else:
+            i += 1
+    for first in first_number:
+        for second in second_number:
+            if first == second:
+                final.append(first)
+
+    if final != []:        
+        return final
+    else:
+        return 0
+            
 
 
 
-if __name__ == "__main__":
-    tripledouble(112266609321, 4455500060669)
+# if __name__ == "__main__":
+print(tripledouble(11226660009321, 44555060669))
