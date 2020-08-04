da9093d96e91b27d970f41bd0e2c6d8d34dc30ae
Cezary Waś <kontakt@cezarywas.pl>
2020-08-04 12:55:10 +0200

    DC#22 final probably

--- Daily Challenge #22 - Simple Pig Latin/pig_latin.py
+++ Daily Challenge #22 - Simple Pig Latin/pig_latin.py
@@ -4,12 +4,15 @@ def pig_latin_converter(input_text):
     Moves first letter of a word to the end of the word and adds "ay".
     """
     input_text = input_text.split()
+    final_words = ""
+
     for words in input_text:
         words = list(words)
         words[-1] = words[0]
         words.remove(words[0])
-        print(f'{words}')
-    return input_text
+        words.append("ay")
+        final_words += "".join(words) + f' '
+    return final_words
 
 if __name__ == "__main__":
     print(pig_latin_converter("Hello there friends"))
