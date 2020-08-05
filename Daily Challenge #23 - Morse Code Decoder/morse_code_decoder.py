c1930a2c01b0291db1e5bc8807fd8dbce1da9b49
Cezary Waś <kontakt@cezarywas.pl>
2020-08-05 11:55:05 +0200

    DC#23 (semi)final(s)

---
+++ Daily Challenge #23 - Morse Code Decoder/morse_code_decoder.py
@@ -0,0 +1,32 @@
+def MORSE_CODE(input_morse):
+    """Morse code converter.
+
+    Can translate from Morse code to letters.
+    """
+    letters_to_morse = { 'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', '/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-'}
+
+    morse_to_letters = { value:key for key, value in letters_to_morse.items()}
+
+    input_morse = input_morse.split()
+
+    result = ""
+    for letter in input_morse:
+        result += morse_to_letters[letter]
+    return result
+
+if __name__ == "__main__":
+    print(MORSE_CODE(".... . -.-- .--- ..- -.. ."))
