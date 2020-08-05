b713238b9efcbac82fa4ab0cfafe9c20e65da57d
Cezary Waś <kontakt@cezarywas.pl>
2020-08-05 13:08:58 +0200

    DC#27 probably done

---
+++ Daily Challenge #27 - Unlucky Days/unlucky_days.py
@@ -0,0 +1,18 @@
+import calendar
+
+def unluckyDays(year):
+    """Friday 13th counter.
+
+    Counts number of Black Fridays in given year.
+    """
+    friday13th = 0
+    cal = calendar.Calendar()
+    for month in range(1,13):
+        for day, week_day in cal.itermonthdays2(year, month):
+            if day == 13 and week_day == 4:
+                friday13th += 1
+
+    return friday13th
+
+if __name__ == "__main__":
+    print(unluckyDays(2015))
