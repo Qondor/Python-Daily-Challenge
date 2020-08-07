fe10639828ec86229df27b817f7a4c70af24a4ad
Cezary Waś <kontakt@cezarywas.pl>
2020-08-07 19:17:58 +0200

    DC#43 final

---
+++ Daily Challenge #43 - Boardgame Fight Resolver/boardgame.py
@@ -0,0 +1,25 @@
+def boardgame_fight(attacker, defender):
+    """Boardgame Fight Resolver.
+
+    Engage in simulation of real life combat. Fighter with bigger number wins and Cavalry wins wih Archers, if numbers are not realated then attackers always wins.
+    """
+    
+    fighters = {
+        "Archers" : 3,
+        "Swordsmen" : 2,
+        "Pikemen" : 1,
+        "Cavalry" : 0
+    }
+
+    key_list = list(fighters.keys())
+    val_list = list(fighters.values())
+    
+    if fighters[attacker] - fighters[defender] == 1 or fighters[attacker] - fighters[defender] == -1:
+        return key_list[val_list.index(max(fighters[attacker], fighters[defender]))]
+    elif fighters[attacker] - fighters[defender] == 3 or fighters[attacker] - fighters[defender] == -3:
+        return "Cavalry"
+    else:
+        return attacker
+
+if __name__ == "__main__":
+    print(boardgame_fight("Archers", "Cavalry"))
