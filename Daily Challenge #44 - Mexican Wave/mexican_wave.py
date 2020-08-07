629d5e4f02c20723e77995e6ee51281f0199e4a8
Cezary Waś <kontakt@cezarywas.pl>
2020-08-07 20:41:53 +0200

    DC#44 probably final

---
+++ Daily Challenge #44 - Mexican Wave/mexican_wave.py
@@ -0,0 +1,20 @@
+def wave(input_text):
+    """Mexican Wave generator.
+
+    Takes a string and returns it changed into Mexican Wave - every letter in sequence gets upppercased.
+    """
+    x = 0
+    temp = []
+    result = []
+    input_text = list(input_text)
+    while x < len(input_text):
+        temp = input_text[:]
+        temp[x] = temp[x].upper()
+        temp = "".join(temp)
+        result.append(temp)
+        x += 1
+
+    return result
+
+if __name__ == "__main__":
+    print(wave("bucciarati"))
