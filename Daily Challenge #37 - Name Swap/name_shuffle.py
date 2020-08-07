522a694c5c45e129549b79d4892bef612277aa9c
Cezary Waś <kontakt@cezarywas.pl>
2020-08-07 15:43:21 +0200

    DC#37 final

---
+++ Daily Challenge #37 - Name Swap/name_shuffle.py
@@ -0,0 +1,11 @@
+def name_shuffler(name):
+    """Name shuffler.
+
+    Changes place of first and last name.
+    """
+
+    name = name.split()
+    return f'{name[-1]} {name[0]}'
+
+if __name__ == "__main__":
+    print(name_shuffler('Emma McClane'))
