eed91550c6a0a4725e4f00233171f5da05174dca
Cezary Waś <kontakt@cezarywas.pl>
2020-08-07 18:07:26 +0200

    DC#40 final

---
+++ Daily Challenge #40 - Counting Sheep/sheeps.py
@@ -0,0 +1,19 @@
+import inflect
+p = inflect.engine()
+
+def sheep_counter(number_of_sheeps):
+    """Sheeps counter.
+
+    Counts sheeps until user falls asleep. 🐑 Baaa!
+    """
+    counter = 2
+    result = "one sheep... \n"
+    while counter <= number_of_sheeps:
+        result += f'{p.number_to_words(counter)} sheeps... \n'
+        counter += 1
+    result += "Zzz..."
+    
+    return result
+
+if __name__ == "__main__":
+    print(sheep_counter(1))
