cb1140e9193313241b2535a18517fce7a422ff94
Cezary Waś <kontakt@cezarywas.pl>
2020-08-07 16:03:38 +0200

    DC#38 final

---
+++ Daily Challenge #38 - Middle Name/middle_name.py
@@ -0,0 +1,16 @@
+def middle_name_shorter(name):
+    """Middle name shorter.
+
+    Takes any name and shortens middle name/names. If name contains only one or two words it is just returned.
+    """
+
+    if len(name.split()) <= 2:
+        return name
+    else:
+        name = name.split()
+        for x in range(1, len(name) - 1):
+            name[x] = f'{name[x][0]}.'
+    return " ".join(name)
+
+if __name__ == "__main__":
+    print(middle_name_shorter('Esteban Julio Ricardo Montoya de la Rosa Ramirez'))
