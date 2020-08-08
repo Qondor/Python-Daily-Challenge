f2bbb2cfc86e5382a1874669c7c78cfa7c1fee72
Cezary Waś <kontakt@cezarywas.pl>
2020-08-08 14:35:23 +0200

    Clearing the code

--- Daily Challenge #21 - Human Readable Time/human_time_converter.py
+++ Daily Challenge #21 - Human Readable Time/human_time_converter.py
@@ -9,50 +9,25 @@ def time_converter(input_seconds):
     output_days = 0
     output_years = 0
 
-    if input_seconds <= 59:
-        return f'{input_seconds} seconds'
+    year_in_seconds = 31536000
+    day_in_seconds = 86400
+    hour_in_seconds = 3600
+    minute_in_seconds = 60
 
-    elif input_seconds <= 3599:
-        while input_seconds >= 60:
-            output_minutes += 1
-            input_seconds -= 60
-        return f'{output_minutes} minutes and {input_seconds} seconds'
+    while input_seconds >= year_in_seconds:
+        output_years += 1
+        input_seconds -= year_in_seconds
+    while input_seconds >= day_in_seconds:
+        output_days += 1
+        input_seconds -= day_in_seconds
+    while input_seconds >= hour_in_seconds:
+        output_hours += 1
+        input_seconds -= hour_in_seconds
+    while input_seconds >= minute_in_seconds:
+        output_minutes += 1
+        input_seconds -= minute_in_seconds
 
-    elif input_seconds <= 86400:
-        while input_seconds >= 3600:
-            output_hours += 1
-            input_seconds -= 3600
-        while input_seconds >= 60:
-            output_minutes += 1
-            input_seconds -= 60
-        return f'{output_hours} hours, {output_minutes} minutes and {input_seconds} seconds'
-
-    elif input_seconds <= 31536000:
-        while input_seconds >= 86400:
-            output_days += 1
-            input_seconds -= 86400
-        while input_seconds >= 3600:
-            output_hours += 1
-            input_seconds -= 3600
-        while input_seconds >= 60:
-            output_minutes += 1
-            input_seconds -= 60
-        return f'{output_days} days, {output_hours} days, {output_minutes} minutes and {input_seconds} seconds'
-
-    else:
-        while input_seconds >= 31536000:
-            output_years += 1
-            input_seconds -= 31536000
-        while input_seconds >= 86400:
-            output_days += 1
-            input_seconds -= 86400
-        while input_seconds >= 3600:
-            output_hours += 1
-            input_seconds -= 3600
-        while input_seconds >= 60:
-            output_minutes += 1
-            input_seconds -= 60
-        return f'{output_years} years, {output_days} days, {output_hours} days, {output_minutes} minutes and {input_seconds} seconds'
+    return f'{output_years} years, {output_days} days, {output_hours} days, {output_minutes} minutes and {input_seconds} seconds' # Small change to the ouptut string, this program always returns all possible time formats even if they are empty
 
 if __name__ == "__main__":
-    print(time_converter(64920192))
+    print(time_converter(62222490))
