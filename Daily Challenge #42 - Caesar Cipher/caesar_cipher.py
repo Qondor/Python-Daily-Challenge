9e74bafabe62d0d728a6c0c67293ed18fe7b9b49
Cezary Waś <kontakt@cezarywas.pl>
2020-08-09 17:35:41 +0200

    Few small tweaks

--- Daily Challenge #42 - Caesar Cipher/caesar_cipher.py
+++ Daily Challenge #42 - Caesar Cipher/caesar_cipher.py
@@ -3,7 +3,6 @@ def caesar_cipher_decrypter(input_text):
 
     Takes any string and moves any letter by 1 in the alphabet - letter "z" turns into "a" to wrap things up.
     """
-
     letter = 0
     result = ""
     input_text = list(input_text)
@@ -11,10 +10,9 @@ def caesar_cipher_decrypter(input_text):
         if input_text[letter].isalpha() == True:
             temp = ord(input_text[letter])
             # Checking if character is letter "z" - if so, it is changed into "a" like true Caesar Cipher would do
-            if temp == 122:
-                temp == 97
-            else:
-                temp += 1
+            temp += 1
+            if temp == 123:
+                temp = 97
             input_text[letter] = chr(temp)
         letter += 1
     return result.join(input_text)
